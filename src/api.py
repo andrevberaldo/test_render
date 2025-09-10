290e144960d62eaf17213e77e58f7db9fc4151ca
André V Beraldo <andreberaldo@hotmail.com>
2025-09-10 10:36:40 -0300

    grafana

--- src/api.py
+++ src/api.py
@@ -1,8 +1,18 @@
 from fastapi import FastAPI
+import os
+from opentelemetry.instrumentation.fastapi import FastAPIInstrumentor
+from opentelemetry.sdk.metrics import MeterProvider
+from opentelemetry.sdk.metrics.export import PeriodicExportingMetricReader
+from opentelemetry.exporter.otlp.proto.http.metric_exporter import OTLPMetricExporter
 
 api = FastAPI(
     title="api to test the render platform"
 )
+
+FastAPIInstrumentor.instrument_app(api)
+exporter = OTLPMetricExporter(endpoint="https://otlb-gateway.grafana.net/otlp", headers={"Authorization": f"Bearer {os.getenv("GRAFANA_TOKEN")}"})
+reader = PeriodicExportingMetricReader(exporter=exporter, export_interval_millis=5000)
+provider = MeterProvider(metric_readers=[reader])
 
 @api.get("/hello")
 def say_hello():
